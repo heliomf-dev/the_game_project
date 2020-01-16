455350a7e7043514cfd6c3c0abd4ecc4e3b22287
Hélio Márcio Filho <heliommsfilho@tutanota.com>
2020-01-16 22:45:46 +0000

    mvn package working

---
+++ src/main/java/com/br/ebserh/modelo/dao/AvaliacaoDAO.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.br.ebserh.modelo.dao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import com.br.ebserh.modelo.planejamento.Avaliacao;
+
+/**
+ *
+ * @author Danillo
+ */
+
+public class AvaliacaoDAO{
+
+    private EntityManagerFactory factory;
+    private EntityManager gerenciador;
+    
+    public AvaliacaoDAO() {
+        factory=Persistence.createEntityManagerFactory("indicadores");
+        gerenciador=factory.createEntityManager();
+    }
+    
+    public boolean adicionar(Avaliacao avaliacao) {
+     boolean sucesso;
+        try
+        {
+           iniciarTransacao();
+           gerenciador.persist(avaliacao);
+           sucesso=true;
+        }
+        catch(Exception e)
+        {
+            sucesso=false;
+            e.printStackTrace();
+        }
+        finally
+        {
+            finalizarTransacao();
+        }
+       return sucesso;
+    }
+    
+    public void iniciarTransacao()
+    {
+         factory=Persistence.createEntityManagerFactory("indicadores");
+         gerenciador=factory.createEntityManager();
+         gerenciador.getTransaction().begin();
+    }
+    
+    public void finalizarTransacao()
+    {
+        
+           gerenciador.getTransaction().commit();
+           gerenciador.close();
+           factory.close();
+    }
+}
